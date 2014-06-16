c16b0a8488e7385c3261f76ed585d170948c1cb5
David Alpert <david@spinthemoose.com>
2014-06-16 03:07:15 -0500

    init now creates the target folder if it doesn't exist

--- FlowDoc.CLI/Commands/InitCommand.cs
+++ FlowDoc.CLI/Commands/InitCommand.cs
@@ -18,22 +18,26 @@ namespace FlowDoc.CLI.Commands
 
         public void Execute()
         {
-            var targetPath = Options.TargetDirectory;
+            var targetPath = new DirectoryInfo(Options.TargetDirectory);
 
+            if (targetPath.Exists == false)
+            {
+                targetPath.Create();
+            }
 
             WriteConfig(targetPath);
             WriteTOC(targetPath);
         }
 
-        private void WriteConfig(string targetPath)
+        private void WriteConfig(DirectoryInfo targetDir)
         {
-            var configPath = Path.Combine(targetPath, ".config");
+            var configPath = Path.Combine(targetDir.FullName, ".config");
             fileSystem.File.WriteAllText(configPath, "# Configure flowdoc settings here");
         }
 
-        private void WriteTOC(string targetPath)
+        private void WriteTOC(DirectoryInfo targetDir)
         {
-            var path = Path.Combine(targetPath, @"toc.md");
+            var path = Path.Combine(targetDir.FullName, @"toc.md");
             fileSystem.File.WriteAllText(path, @"<!---
 Welcome to FlowDoc, a markdown-based documentation system for describing software workflows.
 --->
--- FlowDoc.IntegrationTests/Commands/InitCommandTests.cs
+++ FlowDoc.IntegrationTests/Commands/InitCommandTests.cs
@@ -31,5 +31,28 @@ namespace FlowDoc.IntegrationTests.Commands
                 ObjectApprover.VerifyWithJson(allFiles);
             }
         }
+
+        [Test]
+        public void Execute_against_a_folder_that_does_not_exist()
+        {
+            var currentDir = Environment.CurrentDirectory;
+            var flowExe = Path.Combine(currentDir, "flow.exe");
+            Assert.IsTrue(File.Exists(flowExe));
+
+            using (var rootPath = new DisposableTempFolder())
+            {
+                var targetPath = Path.Combine(rootPath, "foo");
+
+                var args = new string[] {"init", targetPath};
+
+                CLI.Program.Main(args);
+
+                var allFiles = Directory.EnumerateFiles(targetPath, "*.*", SearchOption.AllDirectories)
+                                        .Select(f => f.Replace(targetPath, "~"))
+                                        .ToList();
+
+                ObjectApprover.VerifyWithJson(allFiles);
+            }
+        }
     }
 }

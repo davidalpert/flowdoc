40db08b9998b1a26230fa6c65a51c44d614c4ad2
David Alpert <david@spinthemoose.com>
2014-06-16 02:33:00 -0500

    initial command: flow init

---
+++ FlowDoc.CLI/Commands/InitCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+
+namespace FlowDoc.CLI.Commands
+{
+    public class InitCommand : ICommand<InitOptions>
+    {
+        private readonly IFileSystem fileSystem;
+
+        public InitCommand(IFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public void Execute()
+        {
+            var targetPath = Options.TargetDirectory;
+
+
+            WriteConfig(targetPath);
+            WriteTOC(targetPath);
+        }
+
+        private void WriteConfig(string targetPath)
+        {
+            var configPath = Path.Combine(targetPath, ".config");
+            fileSystem.File.WriteAllText(configPath, "# Configure flowdoc settings here");
+        }
+
+        private void WriteTOC(string targetPath)
+        {
+            var path = Path.Combine(targetPath, @"toc.md");
+            fileSystem.File.WriteAllText(path, @"<!---
+Welcome to FlowDoc, a markdown-based documentation system for describing software workflows.
+--->
+");
+        }
+
+        public InitOptions Options { get; set; }
+    }
+
+    public class InitOptions
+    {
+        public string TargetDirectory { get; set; }
+    }
+
+    public interface ICommand<TOptions>
+    {
+        void Execute();
+        TOptions Options { get; set; }
+    }
+}
---
+++ FlowDoc.Tests/Commands/InitCommandTests.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using ApprovalTests;
+using FlowDoc.CLI.Commands;
+using NUnit.Framework;
+using ObjectApproval;
+
+namespace FlowDoc.Tests.Commands
+{
+    [TestFixture]
+    public class InitCommandTests
+    {
+        [Test]
+        public void Execute()
+        {
+            var targetPath = Path.Combine("c:", "dev", "mydocs");
+
+            var fileSystem = new MockFileSystem();
+
+            var cmd = new InitCommand(fileSystem)
+                      {
+                          Options = new InitOptions()
+                                    {
+                                        TargetDirectory = targetPath
+                                    }
+                      };
+
+            cmd.Execute();
+
+            ObjectApprover.VerifyWithJson(fileSystem.AllFiles);
+        }
+
+        [Test]
+        public void Execute_creates_a_table_of_contents()
+        {
+            var targetPath = Path.Combine("c:", "dev", "mydocs");
+            var tocPath = Path.Combine(targetPath, "toc.md");
+
+            var fileSystem = new MockFileSystem();
+
+            var cmd = new InitCommand(fileSystem)
+                      {
+                          Options = new InitOptions()
+                                    {
+                                        TargetDirectory = targetPath
+                                    }
+                      };
+
+            cmd.Execute();
+
+            Assert.IsTrue(fileSystem.File.Exists(tocPath));
+            Approvals.Verify(fileSystem.File.ReadAllText(tocPath));
+        }
+
+        [Test]
+        public void NewMockFileSystem()
+        {
+            var fileSystem = new MockFileSystem();
+            ObjectApprover.VerifyWithJson(fileSystem);
+        }
+    }
+}
